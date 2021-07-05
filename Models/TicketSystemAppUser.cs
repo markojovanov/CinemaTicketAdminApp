c827b862443d2fd5a8e9706c837144a0a8c18d58
markojovanov999@gmail.com <markojovanov999@gmail.com>
2021-07-05 23:53:05 +0200

    Final commit

---
+++ Models/TicketSystemAppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaTicketAdminApp.Models
+{
+    public class CinemaTicketAdminAppUser
+    {
+        public string Email { get; set; }
+
+        public string NormalizedUserName { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
